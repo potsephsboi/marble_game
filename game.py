a157d7b3c607619cac8ea1984243e2afe9e2dde9
potsephsboi <adpetrak@gmail.com>
2022-09-09 00:25:06 +0300

    show marbles in window

--- game.py
+++ game.py
@@ -1,18 +1,12 @@
-import pygame 
+from helper import *
 
 
-WIDTH = 600
-HEIGHT = 600
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-pygame.init()
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def draw():
     WIN.fill(WHITE)
+    for m in Marble.MARBLES:
+        m.drawMarble()
     pygame.display.update()
 
 def main():
